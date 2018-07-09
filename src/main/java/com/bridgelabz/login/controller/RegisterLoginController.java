8746a091577eff1bd380e4dfdf5de9d30c6da756
Saritaj709 <Saritaj709@gmail.com>
2018-07-09 19:56:31 +0530

    added

--- src/main/java/com/bridgelabz/login/controller/RegisterLoginController.java
+++ src/main/java/com/bridgelabz/login/controller/RegisterLoginController.java
@@ -25,35 +25,6 @@ public class RegisterLoginController {
 	@Autowired
 	private Service userService;
 
-	/*
-	 * @RequestMapping(value = "/login/", method = RequestMethod.POST) public
-	 * ResponseEntity<String> login(@RequestBody User userVerify) throws
-	 * ClassNotFoundException, SQLException { logger.info("Logging User : {}",
-	 * userVerify); System.out.println("login here");
-	 * 
-	 * User user = userService.loginUser(userVerify.getEmail(),
-	 * userVerify.getPassword()); if (user.getEmail() ==
-	 * null||user.getPassword()==null) {
-	 * logger.error("User with email {} not found.", user.getEmail()); return new
-	 * ResponseEntity(new Utility("User with email " + userVerify.getEmail() +
-	 * " not found"), HttpStatus.NOT_FOUND); } String message = "Hello, " +
-	 * user.getFirstname() + "  "+ user.getLastname() + " Email:- " +
-	 * user.getEmail() + " Phone Number:- " + user.getMobileNo(); return new
-	 * ResponseEntity<String>(message, HttpStatus.OK); }
-	 * 
-	 * @RequestMapping(value = "/register/", method = RequestMethod.POST) public
-	 * ResponseEntity<String> register(@RequestBody User user) throws
-	 * ClassNotFoundException{ logger.info("Register user : {}", user);
-	 * 
-	 * boolean registered = userService.registerUser(user); if(!registered) {
-	 * logger.error("User with email {} already present.", user.getEmail()); return
-	 * new ResponseEntity(new Utility("User with email " + user.getEmail() +
-	 * " already present"), HttpStatus.CONFLICT); }
-	 * logger.info("User registered with : {}", user.getEmail()); String message =
-	 * "Successfully registered"; return new ResponseEntity<String>(message,
-	 * HttpStatus.OK);
-	 */
-
 	@RequestMapping("/users")
 	public List<User> getEmployeeDetails() throws ClassNotFoundException {
 		return userService.getUserDetails();
